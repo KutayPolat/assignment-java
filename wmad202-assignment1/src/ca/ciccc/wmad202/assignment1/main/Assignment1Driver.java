872af4541445cad6d9c24e090fa5dc28d11d049c
KutayPolat <polatt.kutay@gmail.com>
2022-06-09 21:21:21 -0700

    :bug: fixed

--- wmad202-assignment1/src/ca/ciccc/wmad202/assignment1/main/Assignment1Driver.java
+++ wmad202-assignment1/src/ca/ciccc/wmad202/assignment1/main/Assignment1Driver.java
@@ -1,11 +1,14 @@
 package ca.ciccc.wmad202.assignment1.main;
 
 
-import ca.ciccc.wmad202.assignment1.question1.Question1;
+import ca.ciccc.wmad202.assignment1.question4.Question4;
+import ca.ciccc.wmad202.assignment1.question5.Question5;
 
 public class Assignment1Driver {
     public static void run() {
-        Question1 question4 = new Question1();
+        Question4 question4 = new Question4();
         question4.sayHelloWorld();
+        Question5 question5 = new Question5();
+        question5.sayHelloWorld();
     }
 }
