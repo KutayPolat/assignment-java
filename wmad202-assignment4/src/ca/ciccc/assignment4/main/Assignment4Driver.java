7f18eac00f18801a59566e9efd7108dfc5886a05
KutayPolat <polatt.kutay@gmail.com>
2022-06-20 21:30:33 -0700

    assignment-4 solution 1-2 question

--- wmad202-assignment4/src/ca/ciccc/assignment4/main/Assignment4Driver.java
+++ wmad202-assignment4/src/ca/ciccc/assignment4/main/Assignment4Driver.java
@@ -1,10 +1,33 @@
 package ca.ciccc.assignment4.main;
 
-import ca.ciccc.assignment4.question9.Question9;
+import ca.ciccc.assignment4.question1.Question1;
+import ca.ciccc.assignment4.question2.Question2;
+
+import java.util.Scanner;
 
 public class Assignment4Driver {
     public static void run() {
-        Question9 question2 = new Question9();
-        question2.invoke();
+        boolean stop = true;
+
+        while (stop){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter question number 1-10 to show solve of question");
+            int questions = scanner.nextInt();
+
+            switch (questions) {
+                case 1:
+                    Question1 question = new Question1();
+                    question.invoke();
+                    stop = false;
+                    break;
+                case 2:
+                    Question2 question2 = new Question2();
+                    question2.invoke();
+                    stop = false;
+                    break;
+                default:
+                    System.out.println("Enter number between 1 to 10");
+            }
+        }
     }
 }
