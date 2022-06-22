345960da1607b5d2de6d68827ee241f76217a3ac
KutayPolat <polatt.kutay@gmail.com>
2022-06-21 17:20:55 -0700

    :fire: finish assignment 4

--- wmad202-assignment4/src/ca/ciccc/assignment4/main/Assignment4Driver.java
+++ wmad202-assignment4/src/ca/ciccc/assignment4/main/Assignment4Driver.java
@@ -2,6 +2,10 @@ package ca.ciccc.assignment4.main;
 
 import ca.ciccc.assignment4.question1.Question1;
 import ca.ciccc.assignment4.question2.Question2;
+import ca.ciccc.assignment4.question3.Question3;
+import ca.ciccc.assignment4.question4.Question4;
+import ca.ciccc.assignment4.question5.Question5;
+import ca.ciccc.assignment4.question6.Question6;
 
 import java.util.Scanner;
 
@@ -23,6 +27,26 @@ public class Assignment4Driver {
                 case 2:
                     Question2 question2 = new Question2();
                     question2.invoke();
+                    stop = false;
+                    break;
+                case 3:
+                    Question3 question3 = new Question3();
+                    question3.invoke();
+                    stop = false;
+                    break;
+                case 4:
+                    Question4 question4 = new Question4();
+                    question4.invoke();
+                    stop = false;
+                    break;
+                case 5:
+                    Question5 question5 = new Question5();
+                    question5.invoke();
+                    stop = false;
+                    break;
+                case 6:
+                    Question6 question6 = new Question6();
+                    question6.invoke();
                     stop = false;
                     break;
                 default:
